4b7e04b8b399e4ae0a487164a243f768130cc394
Lada <bubnovichl@mail.ru>
2022-06-19 18:25:19 +0300

    init project

--- итоговый проект/dist/index.18dbc454.js
+++ итоговый проект/dist/index.18dbc454.js
@@ -527,14 +527,21 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"1SICI":[function(require,module,exports) {
 $(document).ready(function() {
-    $('.slide-content').slick();
-});
-$('.one-time').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
+    $('.slides').slick({
+        arrows: false,
+        dots: true,
+        addaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 300,
+        easing: "linear",
+        infinite: true,
+        initialSlide: 1,
+        autoplay: false,
+        draggable: true,
+        swipe: true,
+        waitForAnimate: false
+    });
 });
 
 },{}]},["54dwi","1SICI"], "1SICI", "parcelRequiref454")
--- итоговый проект/src/js/main.js
+++ итоговый проект/src/js/main.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
-	$('.slide-content').slick();
+	$('.slides').slick({
+		arrows: false,
+		dots: true,
+		addaptiveHeight: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 300,
+		easing: "linear",
+		infinite: true,
+		initialSlide: 1,
+		autoplay: false,
+		draggable: true,
+		swipe: true,
+		waitForAnimate: false,
+	});
 });
 
-$('.one-time').slick({
-	dots: true,
-	infinite: true,
-	speed: 300,
-	slidesToShow: 1,
-	adaptiveHeight: true
-});
